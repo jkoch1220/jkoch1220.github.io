a29ddeed47a94dc0eda7641ab61231b4162349b1
Julian Koch <jtatwcook@gmail.com>
2025-07-31 13:35:23 +0200

    configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  distDir: 'build',
+  trailingSlash: true,
+  basePath: '',
+  assetPrefix: '',
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
